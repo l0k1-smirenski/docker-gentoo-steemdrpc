5372675a68c220d3238388f1785644301651980a
l0k1-smirenski <l0k1@null.net>
2017-04-15 17:49:19 +0200

    fixed config, updated for 18.2 issues (no statistics plugins working)

--- dirtycache.sh
+++ dirtycache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
     echo    75 | sudo tee /proc/sys/vm/dirty_background_ratio
-    echo  1000 | sudo tee /proc/sys/vm/dirty_expire_centisecs
+    echo  10000 | sudo tee /proc/sys/vm/dirty_expire_centisecs
     echo    80 | sudo tee /proc/sys/vm/dirty_ratio
-    echo 30000 | sudo tee /proc/sys/vm/dirty_writeback_centisecs
+    echo 100000 | sudo tee /proc/sys/vm/dirty_writeback_centisecs
